075242aa56fa60be880b5271229b4ffd65892fcd
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-09-03 15:42:04 +0200

    Hey I think this might be my first commit
    a few days before...the due date.....its been a rough block

---
+++ Nav.js
@@ -0,0 +1,2 @@
+let HomeButton = document.getElementById("HomeButton");
+HomeButton.addEventListener('click', function(){location.href = "../index.html";});
---
+++ index.js
@@ -0,0 +1,12 @@
+let NavButton1 = document.getElementsByClassName("NavButton")[0];
+NavButton1.addEventListener('click', function(){location.href = "about/about.html";});
+
+let NavButton2 = document.getElementsByClassName("NavButton")[1];
+NavButton2.addEventListener('click', function(){location.href = "blogs/blogs.html";});
+
+let NavButton3 = document.getElementsByClassName("NavButton")[2];
+NavButton3.addEventListener('click', function(){location.href = "creative/work/creative-work.html";});
+
+let NavButton4 = document.getElementsByClassName("NavButton")[3];
+NavButton4.addEventListener('click', function(){location.href = "wireframe/wireframe.html";});
+

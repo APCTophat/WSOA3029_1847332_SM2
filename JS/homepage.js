6e3d28cc4987087427404ef585fed086fb9404ff
APCTophat <49924900+APCTophat@users.noreply.github.com>
2020-10-21 13:51:47 +0200

    Okay my homepage is done but needs tweaking

--- JS/homepage.js
+++ JS/homepage.js
@@ -1,28 +1,81 @@
 let Display_1 = document.getElementById("Link_Display_1")
 let Text_1 = document.getElementById("Link_Text_1")
 
+let Display_2 = document.getElementById("Link_Display_2")
+let Text_2 = document.getElementById("Link_Text_2")
+
+let Display_3 = document.getElementById("Link_Display_3")
+let Text_3 = document.getElementById("Link_Text_3")
+
+let Display_4 = document.getElementById("Link_Display_4")
+let Text_4 = document.getElementById("Link_Text_4")
+
 
 
 function AddEventListers_1(){
-    console.log("LOAD");
     [Display_1, Text_1].forEach(function(element) {
         
         element.addEventListener('mouseover', function(){
             d3.select('#Link_Display_1').transition().style("r", "calc(30px + 6vw)")
+            d3.select('#Link_Display_1').style("fill", "#bf9645") 
         })
 
         element.addEventListener('mouseout', function(){
-            d3.select('#Link_Display_1').transition().style("r", "calc(20px + 6vw)")
+            d3.select('#Link_Display_1').transition().style("r", "calc(20px + 6vw)") 
+            d3.select('#Link_Display_1').style("fill", "#7d622e")
           })
     }); 
-}
 
+    [Display_2, Text_2].forEach(function(element) {
+        
+        element.addEventListener('mouseover', function(){
+            d3.select('#Link_Display_2').transition().style("r", "calc(30px + 6vw)")
+            d3.select('#Link_Display_2').style("fill", "#bf9645") 
+        })
+
+        element.addEventListener('mouseout', function(){
+            d3.select('#Link_Display_2').transition().style("r", "calc(20px + 6vw)")
+            d3.select('#Link_Display_2').style("fill", "#9c7730")
+          })
+    }); 
+
+    [Display_3, Text_3].forEach(function(element) {
+        
+        element.addEventListener('mouseover', function(){
+            d3.select('#Link_Display_3').transition().style("r", "calc(30px + 6vw)")
+            d3.select('#Link_Display_3').style("fill", "#dead4e") 
+        })
+
+        element.addEventListener('mouseout', function(){
+            d3.select('#Link_Display_3').transition().style("r", "calc(20px + 6vw)")
+            d3.select('#Link_Display_3').style("fill", "#b38734")
+          })
+    }); 
+
+    [Display_4, Text_4].forEach(function(element) {
+        
+        element.addEventListener('mouseover', function(){
+            d3.select('#Link_Display_4').transition().style("r", "calc(30px + 6vw)")
+            d3.select('#Link_Display_4').style("fill", "#dead4e") 
+        })
+
+        element.addEventListener('mouseout', function(){
+            d3.select('#Link_Display_4').transition().style("r", "calc(20px + 6vw)")
+            d3.select('#Link_Display_4').style("fill", "#c7922e")
+          })
+    }); 
+   
+}
 
 
 
 ///Things to load on start
 window.onload = function(){
-    d3.select('#Link_Display_1').style("r", "calc(20px + 6vw)");
+    d3.select('#Link_Display_1').style("fill", "#7d622e")
+    d3.select('#Link_Display_2').style("fill", "#9c7730")
+    d3.select('#Link_Display_3').style("fill", "#b38734")
+    d3.select('#Link_Display_4').style("fill", "#c7922e")
+    d3.selectAll("#Link_Display_1, #Link_Display_2, #Link_Display_3, #Link_Display_4").style("r", "calc(20px + 6vw)");
     AddEventListers_1();
 }
 
